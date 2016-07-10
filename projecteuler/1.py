9bd2026a029cdb71b4b8094f3f206dea723c9e83
linganesan <linganesan.12@cse.mrt.ac.lk>
2016-07-10 13:59:37 +0530

    modify projecteuler_1

--- projecteuler/1.py
+++ projecteuler/1.py
@@ -1,8 +1,34 @@
 import sys
 
+# https://codereview.stackexchange.com/questions/2/project-euler-problem-1-in-python-multiples-of-3-and-5
+def sum_factors_of_n_below_k(k, n):
+    m = (k-1) // n
+    return n * m * (m+1) // 2
+
+def calSum(arg):
+    N = int(arg)
+    return (sum_factors_of_n_below_k(N, 3) +
+            sum_factors_of_n_below_k(N, 5) -
+            sum_factors_of_n_below_k(N, 15))
 if __name__ == '__main__':
     T = int(sys.stdin.readline())
 
     for _ in range(T):
         N = int(sys.stdin.readline())
-        print(sum(i for i in range(3,N) if i%3==0 or i%5==0))
+        print(calSum(N))
+
+# method1
+# print(sum(i for i in range(3,N) if i%3==0 or i%5==0))
+
+
+# method2
+# def cal(limit):
+#     s1, s2 = set(range(0, limit, 3)), set(range(0, limit, 5))
+#     return sum(s1.union(s2))
+#
+# if __name__ == '__main__':
+#     T = int(sys.stdin.readline())
+#
+#     for _ in range(T):
+#         N = int(sys.stdin.readline())
+#         print(cal(N))
