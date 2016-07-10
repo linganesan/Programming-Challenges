a36cab25b471c5a2869b3642dd722a3293d6f247
linganesan <linganesan.12@cse.mrt.ac.lk>
2016-07-10 23:47:08 +0530

    projecteuler_5

--- projecteuler/Project Euler #5: Smallest multiple.py
+++ projecteuler/Project Euler #5: Smallest multiple.py
@@ -1,17 +1,37 @@
 import sys
+import math
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def calSmallestNum(arg):
-    V = int(arg)
-    primelist = []
-    i = 2
-    while i * i <= V:
-        if V % i:
-            primelist.append(i)
+    N = int(arg)
+    primelist = [2,3]
+    # only need to test odd numbers
+    for x in range(5,N+1,2):
+        if is_prime(x):
+            primelist.append(x)
+    newlist = []
+    for a in primelist:
+        i = 1
+        while (a**i<=N):
             i += 1
-        else:
-            V //= i
+            b = a**(i-1)
+        newlist.append(b)
+    result = 1
+    for a in newlist:
+        result *= a
+    return result
 
 if __name__ == '__main__':
     T = int(sys.stdin.readline())
     for _ in range(T):
         N = int(sys.stdin.readline())
-        print(calSmallestNum(N))
+        if N == 2:
+            print(2)
+        elif N ==1:
+            print(1)
+        else:
+            print(calSmallestNum(N))
